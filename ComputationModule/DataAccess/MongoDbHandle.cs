763f237a15708aa1193eee3152cd8d8134f7a43c
smialek <michal.smialek@ee.pw.edu.pl>
2021-10-25 17:52:39 +0200

    MongoDB handle update for GridFS #1

--- ComputationModule/DataAccess/MongoDbHandle.cs
+++ ComputationModule/DataAccess/MongoDbHandle.cs
@@ -8,6 +8,7 @@ using ComputationModule.Messages;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
 using Newtonsoft.Json;
 using Serilog;
 
@@ -18,7 +19,7 @@ namespace ComputationModule.DataAccess
         private readonly string _connectionString;
         private IMongoClient _mongoClient;
         private IMongoDatabase _mongoDatabase;
-        private IMongoCollection<BsonDocument> _mongoCollection;
+        private IGridFSBucket _mongoBucket;
 
         public MongoDbHandle(string pinName, IConfiguration configuration) : base(pinName, configuration)
         {
@@ -33,9 +34,9 @@ namespace ComputationModule.DataAccess
             if ("input" != PinConfiguration.PinType)
                 throw new Exception("Download cannot be called for output pins");
             if (!handle.TryGetValue("Database", out var databaseName))
-                throw new ArgumentException("Incorrect DataHandle.");
+                throw new ArgumentException("Incorrect DataHandle (Database).");
             if (!handle.TryGetValue("Collection", out var collectionName))
-                throw new ArgumentException("Incorrect DataHandle.");
+                throw new ArgumentException("Incorrect DataHandle (Collection).");
 
             Prepare(databaseName, collectionName);
 
@@ -45,21 +46,17 @@ namespace ComputationModule.DataAccess
                 case DataMultiplicity.Single:
                 {
                     if (!handle.TryGetValue("ObjectId", out string id))
-                        throw new ArgumentException("Incorrect DataHandle.");
+                        throw new ArgumentException("Incorrect DataHandle (ObjectId).");
+                    if (!handle.TryGetValue("FileName", out string fileName))
+                        throw new ArgumentException("Incorrect DataHandle (FileName).");
                     try
                     {
                         Log.Information($"Downloading object with id: {id}");
-                        var filter = Builders<BsonDocument>.Filter.Eq("_id", new ObjectId(id));
-                        var document = _mongoCollection.Find(filter).FirstOrDefault();
-                        if (document != null)
-                        {
-                            localPath = DownloadSingleFile(document, LocalPath);
-                            Log.Information($"Downloading object with id: {id} successful.");
-                        }
-                        else
-                        {
-                            Log.Information($"Can not find object with id {id}");
-                        }
+
+                        localPath = $"{LocalPath}/{fileName}";
+                        DownloadOneFile(ObjectId.Parse(id), localPath);
+                        
+                        Log.Information($"Downloading object with id: {id} successful.");
                     }
                     catch (Exception)
                     {
@@ -77,11 +74,11 @@ namespace ComputationModule.DataAccess
                         Log.Information($"Downloading all files from {collectionName}.");
                         localPath = $"{LocalPath}/{collectionName}";
                         Directory.CreateDirectory(localPath);
-                        var filter = Builders<BsonDocument>.Filter.Empty;
-                        var documents = _mongoCollection.Find(filter).ToList();
 
-                        foreach (var document in documents)
-                            DownloadSingleFile(document, localPath);
+                        using var cursor = _mongoBucket.Find(Builders<GridFSFileInfo>.Filter.Empty);
+                        
+                        foreach (var file in cursor.ToList())
+                            DownloadOneFile(file.Id, localPath + "/" + file.Filename);
 
                         AddGuidToFilesName(localPath);
                         Log.Information($"Downloading all files from {collectionName} successful.");
@@ -98,6 +95,13 @@ namespace ComputationModule.DataAccess
             }
 
             return localPath;
+        }
+        
+        private void DownloadOneFile(ObjectId id, string localPath)
+        {
+            FileStream file = new FileStream(localPath,FileMode.Create);
+            _mongoBucket.DownloadToStream(id, file);
+            file.Close();
         }
 
         public override Dictionary<string, string> Upload(string localPath)
@@ -123,27 +127,21 @@ namespace ComputationModule.DataAccess
                     {
                         Log.Information($"Uploading file from {localPath} to collection {collectionName}");
 
-                        var bsonDocument = GetBsonDocument(localPath);
-                        _mongoCollection.InsertOne(bsonDocument);
-
-                        handle = GetTokenHandle(bsonDocument);
+                        handle = UploadOneFile(localPath);
                         handle.Add("Database", databaseName);
                         handle.Add("Collection", collectionName);
-
+                        
                         Log.Information($"Upload file from {localPath} successful.");
                         break;
                     }
                     case DataMultiplicity.Multiple:
                     {
-                        Log.Information($"Uploading directory from {localPath} to collection {collectionName}");
+                        Log.Information($"Uploading directory from {localPath} to bucket {collectionName}");
                         var files = GetAllFiles(localPath);
                         var handleList = new List<Dictionary<string, string>>();
 
-                        foreach (var bsonDocument in files.Select(file => GetBsonDocument(file.FullName)))
-                        {
-                            _mongoCollection.InsertOne(bsonDocument);
-                            handleList.Add(GetTokenHandle(bsonDocument));
-                        }
+                        foreach (FileInfo fileInfo in GetAllFiles(localPath))
+                            handleList.Add(UploadOneFile(fileInfo.FullName));
 
                         handle = new Dictionary<string, string>
                         {
@@ -168,6 +166,20 @@ namespace ComputationModule.DataAccess
             {
                 ClearLocal();
             }
+        }
+
+        private Dictionary<string, string> UploadOneFile(string localPath)
+        {
+            FileStream file = new FileStream(localPath, FileMode.Open);
+            string fileName = new FileInfo(localPath).Name;
+            ObjectId id = _mongoBucket.UploadFromStream(fileName, file);
+            file.Close();
+                        
+            return new Dictionary<string, string>()
+            {
+                {"ObjectId", id.ToString()}, 
+                {"FileName", fileName}
+            };
         }
 
         public override short CheckConnection(Dictionary<string, string> handle = null)
@@ -220,19 +232,20 @@ namespace ComputationModule.DataAccess
                         return -3;
                     }
 
-                    _mongoCollection = _mongoDatabase.GetCollection<BsonDocument>(collectionName);
-                    if (_mongoCollection == null)
+                    _mongoBucket = new GridFSBucket(_mongoDatabase,
+                        new GridFSBucketOptions() {BucketName = collectionName});
+                    var cursor = _mongoBucket.Find(Builders<GridFSFileInfo>.Filter.Empty);
+                    if (0 == cursor.ToList().Count)
                     {
-                        Log.Error($"No collection {collectionName}");
+                        Log.Error($"No or empty bucket {collectionName}");
                         return -3;
                     }
 
                     if (PinConfiguration.DataMultiplicity == DataMultiplicity.Single)
                     {
-                        var filter = Builders<BsonDocument>.Filter.Eq("_id", new ObjectId(id));
-                        var document = _mongoCollection.Find(filter).FirstOrDefault();
+                        cursor = _mongoBucket.Find(Builders<GridFSFileInfo>.Filter.Eq(x => x.Id, ObjectId.Parse(id)));
 
-                        if (document == null)
+                        if (0 == cursor.ToList().Count)
                         {
                             Log.Error($"No document with id {id}");
                             return -3;
@@ -259,20 +272,8 @@ namespace ComputationModule.DataAccess
             //TODO to reset or not to reset
             _mongoClient = new MongoClient(_connectionString);
             _mongoDatabase = _mongoClient.GetDatabase(databaseName);
-            _mongoCollection = _mongoDatabase.GetCollection<BsonDocument>(collectionName);
+            _mongoBucket = new GridFSBucket(_mongoDatabase, new GridFSBucketOptions(){BucketName = collectionName});
             return (databaseName, collectionName);
-        }
-
-        private static string DownloadSingleFile(BsonDocument document, string localPath)
-        {
-            var fileName = document.GetElement("fileName").Value.AsString;
-            var fileContent = document.GetElement("fileContent").Value.AsBsonBinaryData;
-            var filePath = $"{localPath}/{fileName}";
-            using var fileStream = File.OpenWrite(filePath);
-            fileStream.Write(fileContent.Bytes);
-            fileStream.Dispose();
-
-            return filePath;
         }
 
         private static BsonDocument GetBsonDocument(string localPath)
